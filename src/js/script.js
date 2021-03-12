52eb722ee9ecdd667bff062ee771a9c4257a081b
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-03-12 15:20:10 +0100

    Add responsive menu in RWD

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu(visible) {
+  document.querySelector('.menu').classList.toggle('show', visible);
+}
+document.querySelector('.hamburger').addEventListener('click', function(e) {
+  e.preventDefault();
+  toggleMenu();
+});
